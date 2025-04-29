2d2d37ce0fe96c06bced52a7eb6dd0b0e108a94c
Anthony Avila <anthonya@americomis.com>
2025-04-29 12:24:11 -0500

    Fixed spelling Error and updated lsits 4/29/25

--- updateLists.ps1
+++ updateLists.ps1
@@ -1,2 +1,2 @@
 ### 3xktech.cloud
-Invoke-WebRequest -Uri "https://asn.ipinfo.app/api/text/list/AS200373" -outfile .\3kxtech.txt -UseBasicParsing
+Invoke-WebRequest -Uri "https://asn.ipinfo.app/api/text/list/AS200373" -outfile .\3xktech.txt -UseBasicParsing
