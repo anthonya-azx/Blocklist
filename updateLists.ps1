736f62d1bfae3941651dad2cc04d1b86d6768c0c
Anthony Avila <anthonya@americomis.com>
2025-05-27 12:20:37 -0500

    Updated and added new list

--- updateLists.ps1
+++ updateLists.ps1
@@ -1,2 +1,3 @@
 ### 3xktech.cloud
 Invoke-WebRequest -Uri "https://asn.ipinfo.app/api/text/list/AS200373" -outfile .\3xktech.txt -UseBasicParsing
+Invoke-WebRequest -Uri "https://asn.ipinfo.app/api/text/list/AS215540" -outfile .\globconnex.txt -UseBasicParsing
